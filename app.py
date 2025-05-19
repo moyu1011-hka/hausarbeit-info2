b9019598852c6411502f8698049d08729ace8e24
moyu1011-hka <moyu1011@h-ka.de>
2025-05-19 18:25:51 +0200

    changing the app and question3 files

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
+from question3_functions import get_top_5_coins, get_volume_data, plot_pie_chart
 import streamlit as st
 import requests
 import matplotlib.pyplot as plt
 
-# UI
-st.title("Анализ популярных криптовалют")
-coin = st.selectbox("Выберите криптовалюту", ["bitcoin", "ethereum", "solana", "ripple", "cardano"])
+# Programming the Frontend
 
-# Получение данных с CoinGecko
+st.title("Analysis of the most popular cryptocurrencies over the past 30 days")
+
+#show the pie chart
+coins = get_top_5_coins()
+labels, volumes = get_volume_data(coins)
+
+if volumes and sum(volumes) > 0:
+    fig = plot_pie_chart(labels, volumes)
+    st.pyplot(fig)
+else:
+    st.warning("No volume data available to plot.")
+
+
+coin = st.selectbox("Choose the currency", ["bitcoin", "ethereum", "solana", "ripple", "cardano"])
+# get data from CoinGecko
 url = f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart"
 params = {"vs_currency": "usd", "days": "30"}
 response = requests.get(url, params=params).json()
-
-# Парсим данные
+# parse data
 prices = response["prices"]
 dates = [x[0] for x in prices]
 values = [x[1] for x in prices]
-
-# График
+#show the line graph
 fig, ax = plt.subplots()
 ax.plot(values)
-ax.set_title(f"Цена {coin} за 30 дней")
+ax.set_title(f"Cost of {coin} for the last 30 days")
 st.pyplot(fig)
